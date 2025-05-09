35e4649669d53214790e71ee99d11104762f6118
Enzo Cañete <enzodev4@gmail.com>
2025-05-09 18:38:10 -0300

    Updated Header and CartBtn components.

--- src/components/CartBtn/CartBtn.tsx
+++ src/components/CartBtn/CartBtn.tsx
@@ -1,23 +1,10 @@
+import './cartBtn.css';
 import { ShoppingCart } from 'lucide-react';
-/* const Btn = styled.button`
-	background-color: var(--dark-green);
-	padding: 10px 10px;
-	border: none;
-	border-radius: 10px;
-	display: flex;
-	gap: 15px;
-	align-items: center;
-`;
- */
-/* const Icon = styled(ShoppingCart)`
-	width: 1.3em;
-	height: 1.3em;
-`;
- */
+
 export const CartBtn = () => {
 	return (
 		<button className='hover'>
-			<ShoppingCart />N
+			<ShoppingCart className='icon' />N
 		</button>
 	);
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,32 @@
 import { CartBtn } from '../index';
+import './header.css';
 
 export const Header = () => {
 	return (
-		<header>
-			<CartBtn></CartBtn>
+		<header className='wrapper'>
+			<span className='logo'>Libri.</span>
+			<nav>
+				<ul>
+					<li>
+						<a href='#' className='hover'>
+							Inicio
+						</a>
+					</li>
+					<li>
+						<a href='#' className='hover'>
+							Productos
+						</a>
+					</li>
+					<li>
+						<a href='#' className='hover'>
+							Contacto
+						</a>
+					</li>
+					<li>
+						<CartBtn />
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
