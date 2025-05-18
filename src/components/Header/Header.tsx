eb8e6fa0259eea092887776ce1257990df6d92e1
Enzo Cañete <enzodev4@gmail.com>
2025-05-18 16:28:37 -0300

    Improved wrapper class.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,30 +3,32 @@ import './header.css';
 
 export const Header = () => {
 	return (
-		<header className='wrapper'>
-			<span className='logo'>Libri.</span>
-			<nav>
-				<ul>
-					<li>
-						<a href='#' className='hover'>
-							Inicio
-						</a>
-					</li>
-					<li>
-						<a href='#' className='hover'>
-							Productos
-						</a>
-					</li>
-					<li>
-						<a href='#' className='hover'>
-							Contacto
-						</a>
-					</li>
-					<li>
-						<CartBtn />
-					</li>
-				</ul>
-			</nav>
+		<header>
+			<div className='wrapper'>
+				<span className='logo'>Libri.</span>
+				<nav>
+					<ul>
+						<li>
+							<a href='#' className='hover'>
+								Inicio
+							</a>
+						</li>
+						<li>
+							<a href='#' className='hover'>
+								Productos
+							</a>
+						</li>
+						<li>
+							<a href='#' className='hover'>
+								Contacto
+							</a>
+						</li>
+						<li>
+							<CartBtn />
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,9 +3,10 @@ import './hero.css';
 export const Hero = () => {
 	return (
 		<section className='hero'>
-			<h1 className='text-wrapper'>libri.</h1>
-
-			<p className='subtitle'>Tu librería en línea. Compra, lee y disfruta.</p>
+			<div className="wrapper">
+				<h1 className='text-wrapper'>libri.</h1>
+				<p className='subtitle'>Tu librería en línea. Compra, lee y disfruta.</p>
+			</div>
 		</section>
 	);
 };
