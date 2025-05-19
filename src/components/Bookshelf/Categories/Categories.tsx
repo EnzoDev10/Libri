1aa3b23027fa30b63ff659e64c242b001272c764
Enzo Cañete <enzodev4@gmail.com>
2025-05-19 20:48:13 -0300

    Removed categories from the book nav.

--- src/components/Bookshelf/Categories/Categories.tsx
+++ src/components/Bookshelf/Categories/Categories.tsx
@@ -10,14 +10,6 @@ export const Categories = () => {
 					<input type='radio' id='Todos' name='categories' />
 					<label htmlFor='All'>Todos</label>
 				</li>
-				<li>
-					<input type='radio' id='Ciencia' name='categories' />
-					<label htmlFor='Ciencia'>Ciencia Ficción</label>
-				</li>
-				<li>
-					<input type='radio' id='Romance' name='categories' />
-					<label htmlFor='Romance'>Romance</label>
-				</li>
 			</ul>
 		</article>
 	);
