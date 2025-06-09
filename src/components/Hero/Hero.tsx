dd00ff4fd3d2d77ad26012d104cc4aaa28602cc1
Enzo <Enzodev4@gmail.com>
2025-06-09 16:41:46 -0300

    Refactored Hero to use styled-components.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,44 @@
-import './Hero.css';
+import styled from "styled-components";
+
+import { Wrapper } from "../Misc";
+
+const HeroSection = styled.section`
+    background-color: var(--hero-bg);
+    padding: 50px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledWrapper = styled(Wrapper)`
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+`;
+
+const TextWrapper = styled.h1`
+    font-size: 6.2rem;
+    font-weight: 700;
+`;
+
+const Subtitle = styled.p`
+    color: #ccc;
+    font-size: 20px;
+    font-style: italic;
+`;
 
 export const Hero = () => {
-	return (
-		<section className='hero'>
-			<div className="wrapper">
-				<h1 className='text-wrapper'>libri.</h1>
-				<p className='subtitle'>Tu librería en línea. Compra, lee y disfruta.</p>
-			</div>
-		</section>
-	);
+    return (
+        <HeroSection>
+            <StyledWrapper>
+                <TextWrapper>libri.</TextWrapper>
+                <Subtitle>
+                    Tu librería en línea. Compra, lee y disfruta.
+                </Subtitle>
+            </StyledWrapper>
+        </HeroSection>
+    );
 };
