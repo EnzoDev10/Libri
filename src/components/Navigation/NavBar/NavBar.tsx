b7201b3189646f46366ee315ec3354f1aec2bb83
Enzo <Enzodev4@gmail.com>
2025-06-11 11:48:10 -0300

    Refactored NavBar to be shorter.

--- src/components/Navigation/NavBar/NavBar.tsx
+++ src/components/Navigation/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../index";
-
 import { UseCart } from "../../../context/CartContext";
+import { ShoppingCart } from "lucide-react";
 interface Prop {
     className: string;
     variant: "header" | "footer";
@@ -10,57 +10,32 @@ interface Prop {
 export const NavBar = ({ variant, className }: Prop) => {
     const { cartContent } = UseCart();
 
-    if (variant == "header") {
-        return (
-            <nav className={className}>
-                <ul>
-                    <li>
-                        <Link to='/' className='hover'>
-                            Inicio
-                        </Link>
-                    </li>
+    return (
+        <nav className={className}>
+            {variant == "footer" && <span className='logo'>Libri.</span>}
 
-                    <li>
-                        <Link to='/' className='hover'>
-                            Contacto
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/carrito'>
-                            <Button
-                                className='border'
-                                Icon='cart'
-                                parentMethod={() => {}}
-                            >
-                                {cartContent.length}
-                            </Button>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        );
-    } else {
-        return (
-            <nav className={className}>
-                <span className='logo'>Libri.</span>
-                <ul>
-                    <li>
-                        <Link to='/' className='hover'>
-                            Inicio
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/' className='hover'>
-                            Contacto
-                        </Link>
-                    </li>
-                </ul>
-                <Link to='/carrito' className='hover'>
-                    <Button className='border' Icon='cart'>
-                        {cartContent.length}
-                    </Button>
-                </Link>
-            </nav>
-        );
-    }
+            <ul>
+                <li>
+                    <Link to='/' className='hover'>
+                        Inicio
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='/' className='hover'>
+                        Contacto
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/carrito'>
+                        <Button className='border' parentMethod={() => {}}>
+                            <ShoppingCart />
+
+                            {cartContent.length}
+                        </Button>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
